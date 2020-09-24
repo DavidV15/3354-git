2d21c3d8c8dbc827fc5a0885f9470b7f0da92ec5
David VanNatta <vannattad92@gmail.com>
2020-09-24 18:37:09 -0500

    Updated Main Functionality
    Adjusted main to function better, still unable to get exception to ignore - for subtraction.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,15 +1,17 @@
-package adder;
+//package adder;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             int result = addArguments(args);
+			if(args.length < 2)
+				throw new ArrayIndexOutOfBoundsException();
             System.out.println(result);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println("Please provide at least two integers to add");
-        } catch (NumberFormatException e){
-			System.err.println("Please enter integers only");
+            System.err.println("Please provide at least two integers to add.");
+        } catch (NumberFormatException e) {
+			System.err.println("Invalid character format.");
 		}
     }
 
@@ -22,7 +24,7 @@ public class Main {
 			sign = 1;
 			index = 1;
 		}
-		for(int i=2;i<args.length;i++){
+		for(int i=index;i<args.length;i++){
 			if(sign==0)
 				sum+=Integer.valueOf(args[i]);
 			if(sign==1)
